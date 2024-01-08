4faafa4b42155be8b2fe3a4c50dbcc82b112507d
TingSyuanWang <vincent@fishboneapps.com>
2024-01-08 01:16:14 -0500

    refactor: make the code more concise
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-wp-json-exporter-api.php
+++ admin/classes/class-wp-json-exporter-api.php
@@ -14,42 +14,14 @@ if ( ! class_exists( 'WP_Json_Exporter_API' ) ) {
 		}
 
 		function register_api(): void {
-			register_rest_route( $this->namespace, '/posts', array(
-				'methods'             => 'GET',
-				'callback'            => array( $this, 'get_posts' ),
-				'permission_callback' => '__return_true',
-			) );
-
-			register_rest_route( $this->namespace, '/posts/(?P<slug>[a-zA-Z0-9-]+)', array(
-				'methods'             => 'GET',
-				'callback'            => array( $this, 'get_post' ),
-				'permission_callback' => '__return_true',
-			) );
-
-			register_rest_route( $this->namespace, '/projects', array(
-				'methods'             => 'GET',
-				'callback'            => array( $this, 'get_projects' ),
-				'permission_callback' => '__return_true',
-			) );
-
-			register_rest_route( $this->namespace, '/projects/(?P<slug>[a-zA-Z0-9-]+)', array(
-				'methods'             => 'GET',
-				'callback'            => array( $this, 'get_project' ),
-				'permission_callback' => '__return_true',
-			) );
+			$this->register_route( '/posts', 'get_posts' );
+			$this->register_route( '/posts/(?P<slug>[a-zA-Z0-9-]+)', 'get_post' );
+			$this->register_route( '/projects', 'get_projects' );
+			$this->register_route( '/projects/(?P<slug>[a-zA-Z0-9-]+)', 'get_project' );
 		}
 
 		function get_posts( $request ): array {
 			$page = $request->get_param( 'page' ) ? (int) $request->get_param( 'page' ) : 1;
-
-			$count_args = array(
-				'post_type'      => 'post',
-				'posts_per_page' => - 1,
-			);
-
-			$count_query = new WP_Query( $count_args );
-			$total_posts = $count_query->found_posts;
-			$total_pages = ceil( $total_posts / $this->posts_per_page );
 
 			$args = array(
 				'post_type'           => 'post',
@@ -64,20 +36,17 @@ if ( ! class_exists( 'WP_Json_Exporter_API' ) ) {
 			$data  = [];
 
 			foreach ( $query->posts as $post ) {
-				$data[] = [
-					'title'          => $post->post_title,
-					'slug'           => $post->post_name,
-					'featured_image' => get_the_post_thumbnail_url( $post->ID, 'full' ),
-					'category'       => str_replace( $this->wordpress_url, $this->redirect_url, get_the_category_list( ', ', '', $post->ID ) ),
-					'date'           => get_the_date( 'd M / Y', $post->ID ),
-				];
+				$data[] = $this->get_post_data( $post, 'post', false );
 			}
+
+			$pagination_data = $this->get_pagination_data( 'post', $page );
 
 			return [
 				'data' => $data,
 				'meta' => [
-					'current_page' => $page,
-					'total_pages'  => $total_pages,
+					'current_page' => $pagination_data['current_page'],
+					'total_pages'  => $pagination_data['total_pages'],
+					'total_posts'  => $pagination_data['total_posts'],
 				]
 			];
 		}
@@ -103,38 +72,17 @@ if ( ! class_exists( 'WP_Json_Exporter_API' ) ) {
 			$previous_post      = $this->get_adjacent_post_custom( $post->post_date, 'post', 'prev' );
 			$previous_post_data = null;
 			if ( $previous_post ) {
-				$previous_post_data = array(
-					'title'          => $previous_post->post_title,
-					'slug'           => $previous_post->post_name,
-					'featured_image' => get_the_post_thumbnail_url( $previous_post->ID, 'full' ),
-					'category'       => str_replace( $this->wordpress_url, $this->redirect_url, get_the_category_list( ', ', '', $previous_post->ID ) ),
-					'date'           => get_the_date( 'd M / Y', $previous_post->ID ),
-				);
+				$previous_post_data = $this->get_post_data( $previous_post, 'post', false );
 			}
 
 			$next_post      = $this->get_adjacent_post_custom( $post->post_date, 'post', 'next' );
 			$next_post_data = null;
 			if ( $next_post ) {
-				$next_post_data = array(
-					'title'          => $next_post->post_title,
-					'slug'           => $next_post->post_name,
-					'featured_image' => get_the_post_thumbnail_url( $next_post->ID, 'full' ),
-					'category'       => str_replace( $this->wordpress_url, $this->redirect_url, get_the_category_list( ', ', '', $next_post->ID ) ),
-					'date'           => get_the_date( 'd M / Y', $next_post->ID ),
-				);
+				$next_post_data = $this->get_post_data( $next_post, 'post', false );
 			}
 
 			return array(
-				'data' => array(
-					'title'         => $post->post_title,
-					'post_date'     => get_the_date( 'd M / Y', $post->ID ),
-					'updated_date'  => get_the_modified_date( 'd M / Y', $post->ID ),
-					'feature_image' => get_the_post_thumbnail_url( $post->ID, 'full' ),
-					'content'       => apply_filters( 'the_content', $post->post_content ),
-					'excerpt'       => get_the_excerpt( $post->ID ),
-					'tags'          => wp_get_post_tags( $post->ID, array( 'fields' => 'names' ) ),
-					'categories'    => wp_get_post_categories( $post->ID, array( 'fields' => 'names' ) )
-				),
+				'data' => $this->get_post_data( $post ),
 				'prev' => $previous_post_data,
 				'next' => $next_post_data,
 			);
@@ -142,15 +90,6 @@ if ( ! class_exists( 'WP_Json_Exporter_API' ) ) {
 
 		function get_projects( $request ): array {
 			$page = $request->get_param( 'page' ) ? (int) $request->get_param( 'page' ) : 1;
-
-			$count_args = array(
-				'post_type'      => 'project',
-				'posts_per_page' => - 1,
-			);
-
-			$count_query = new WP_Query( $count_args );
-			$total_posts = $count_query->found_posts;
-			$total_pages = ceil( $total_posts / $this->posts_per_page );
 
 			$args = array(
 				'post_type'           => 'project',
@@ -165,40 +104,22 @@ if ( ! class_exists( 'WP_Json_Exporter_API' ) ) {
 			$data  = [];
 
 			foreach ( $query->posts as $post ) {
-				$terms      = get_the_terms( $post->ID, 'project_category' );
-				$categories = [];
-
-				if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
-					foreach ( $terms as $term ) {
-						$term_link = get_term_link( $term );
-						if ( ! is_wp_error( $term_link ) ) {
-							$categories[] = '<a href="' . esc_url( $term_link ) . '">' . esc_html( $term->name ) . '</a>';
-						}
-					}
-				}
-
-				$category_list        = implode( ', ', $categories );
-				$custom_category_list = str_replace( $this->wordpress_url, $this->redirect_url, $category_list );
-
-				$data[] = [
-					'title'          => $post->post_title,
-					'slug'           => $post->post_name,
-					'featured_image' => get_the_post_thumbnail_url( $post->ID, 'full' ),
-					'category'       => $custom_category_list,
-					'date'           => get_the_date( 'd M / Y', $post->ID ),
-				];
+				$data[] = $this->get_post_data( $post, 'project', false );
 			}
+
+			$pagination_data = $this->get_pagination_data( 'project', $page );
 
 			return [
 				'data' => $data,
-				'meta' => [
-					'current_page' => $page,
-					'total_pages'  => $total_pages,
-				]
+				'meta' => array(
+					'current_page' => $pagination_data['current_page'],
+					'total_pages'  => $pagination_data['total_pages'],
+					'total_posts'  => $pagination_data['total_posts'],
+				)
 			];
 		}
 
-		function get_project( $request ) {
+		function get_project( $request ): array|WP_Error {
 			$slug = $request['slug'];
 
 			$args = array(
@@ -219,9 +140,31 @@ if ( ! class_exists( 'WP_Json_Exporter_API' ) ) {
 			$next_post      = $this->get_adjacent_post_custom( $post->post_date, 'project', 'next' );
 			$next_post_data = null;
 			if ( $next_post ) {
-				$terms      = get_the_terms( $post->ID, 'project_category' );
-				$categories = [];
+				$next_post_data = $this->get_post_data( $next_post, 'project', false );
+			}
 
+			return array(
+				'data' => $this->get_post_data( $post, 'project' ),
+				'next' => $next_post_data,
+			);
+		}
+
+		private function register_route( $route, $callback ): void {
+			register_rest_route( $this->namespace, $route, array(
+				'methods'             => 'GET',
+				'callback'            => array( $this, $callback ),
+				'permission_callback' => '__return_true',
+			) );
+		}
+
+		private function get_post_data( $post, $type = 'post', $show_detail = true ): array {
+			$categories    = [];
+			$category_list = '';
+
+			if ( $type === 'post' ) {
+				$category_list = get_the_category_list( ', ', '', $post->ID );
+			} else if ( $type === 'project' ) {
+				$terms = get_the_terms( $post->ID, 'project_category' );
 				if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
 					foreach ( $terms as $term ) {
 						$term_link = get_term_link( $term );
@@ -230,31 +173,49 @@ if ( ! class_exists( 'WP_Json_Exporter_API' ) ) {
 						}
 					}
 				}
-
-				$category_list        = implode( ', ', $categories );
-				$custom_category_list = str_replace( $this->wordpress_url, $this->redirect_url, $category_list );
-
-				$next_post_data = array(
-					'title'          => $next_post->post_title,
-					'slug'           => $next_post->post_name,
-					'featured_image' => get_the_post_thumbnail_url( $next_post->ID, 'full' ),
-					'category'       => $custom_category_list,
-					'date'           => get_the_date( 'd M / Y', $next_post->ID ),
-				);
+				$category_list = implode( ', ', $categories );
 			}
 
+			$custom_category_list = str_replace( $this->wordpress_url, $this->redirect_url, $category_list );
+
+			if ( $show_detail ) {
+				return [
+					'title'          => $post->post_title,
+					'slug'           => $post->post_name,
+					'featured_image' => get_the_post_thumbnail_url( $post->ID, 'full' ),
+					'category'       => $custom_category_list,
+					'date'           => get_the_date( 'd M / Y', $post->ID ),
+					'content'        => apply_filters( 'the_content', $post->post_content ),
+					'excerpt'        => get_the_excerpt( $post->ID ),
+					'tags'           => wp_get_post_tags( $post->ID, array( 'fields' => 'names' ) ),
+				];
+			} else {
+				return [
+					'title'          => $post->post_title,
+					'slug'           => $post->post_name,
+					'featured_image' => get_the_post_thumbnail_url( $post->ID, 'full' ),
+					'category'       => $custom_category_list,
+					'date'           => get_the_date( 'd M / Y', $post->ID ),
+				];
+			}
+		}
+
+		private function get_pagination_data( $post_type, $current_page = 1 ): array {
+			$count_args = array(
+				'post_type'      => $post_type,
+				'posts_per_page' => - 1,
+				'fields'         => 'ids', // optimize query for performance
+			);
+
+			$count_query  = new WP_Query( $count_args );
+			$total_posts  = $count_query->found_posts;
+			$total_pages  = ceil( $total_posts / $this->posts_per_page );
+			$current_page = max( 1, min( $current_page, $total_pages ) );
+
 			return array(
-				'data' => array(
-					'title'         => $post->post_title,
-					'post_date'     => get_the_date( 'd M / Y', $post->ID ),
-					'updated_date'  => get_the_modified_date( 'd M / Y', $post->ID ),
-					'feature_image' => get_the_post_thumbnail_url( $post->ID, 'full' ),
-					'content'       => apply_filters( 'the_content', $post->post_content ),
-					'excerpt'       => get_the_excerpt( $post->ID ),
-					'tags'          => wp_get_post_tags( $post->ID, array( 'fields' => 'names' ) ),
-					'categories'    => wp_get_post_categories( $post->ID, array( 'fields' => 'names' ) )
-				),
-				'next' => $next_post_data,
+				'current_page' => $current_page,
+				'total_pages'  => $total_pages,
+				'total_posts'  => $total_posts,
 			);
 		}
 
