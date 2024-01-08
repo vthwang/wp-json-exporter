bb1e5ba1c97b952a6c145e3d5fca42f54d289c05
TingSyuanWang <vincent@fishboneapps.com>
2024-01-07 21:13:11 -0500

    chore: format the code
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-wp-json-exporter-admin-page.php
+++ admin/classes/class-wp-json-exporter-admin-page.php
@@ -3,14 +3,14 @@
 if ( ! class_exists( 'WP_Json_Exporter_Admin_Page' ) ) {
 	class WP_Json_Exporter_Admin_Page {
 		function __construct() {
-            add_action( 'admin_init', array( $this, 'wp_json_exporter_settings_init' ) );
+			add_action( 'admin_init', array( $this, 'wp_json_exporter_settings_init' ) );
 			add_action( 'admin_menu', array( $this, 'add_menu' ) );
 		}
 
-        function wp_json_exporter_settings_init(): void {
-	        register_setting('wp-json-exporter-settings', 'wp_json_exporter_is_redirect');
-	        register_setting('wp-json-exporter-settings', 'wp_json_exporter_redirect_url');
-        }
+		function wp_json_exporter_settings_init(): void {
+			register_setting( 'wp-json-exporter-settings', 'wp_json_exporter_is_redirect' );
+			register_setting( 'wp-json-exporter-settings', 'wp_json_exporter_redirect_url' );
+		}
 
 		function add_menu(): void {
 			add_menu_page(
@@ -26,27 +26,31 @@ if ( ! class_exists( 'WP_Json_Exporter_Admin_Page' ) ) {
 
 		function wp_json_exporter_settings_page(): void {
 			?>
-			<div class="wrap">
-				<h2>WordPress JSON Exporter Settings</h2>
+            <div class="wrap">
+                <h2>WordPress JSON Exporter Settings</h2>
 
-				<form method="post" action="options.php">
-					<?php settings_fields('wp-json-exporter-settings'); ?>
-					<?php do_settings_sections('wp-json-exporter-settings'); ?>
+                <form method="post" action="options.php">
+					<?php settings_fields( 'wp-json-exporter-settings' ); ?>
+					<?php do_settings_sections( 'wp-json-exporter-settings' ); ?>
 
-					<table class="form-table">
-						<tr>
-							<th scope="row">Is Redirect:</th>
-							<td><input type="checkbox" name="wp_json_exporter_is_redirect" <?php checked(get_option('wp_json_exporter_is_redirect'), 1); ?> value="1"></td>
-						</tr>
-						<tr>
-							<th scope="row">Redirect URL:</th>
-							<td><input type="text" name="wp_json_exporter_redirect_url" value="<?php echo esc_attr(get_option('wp_json_exporter_redirect_url')); ?>"></td>
-						</tr>
-					</table>
+                    <table class="form-table">
+                        <tr>
+                            <th scope="row">Is Redirect:</th>
+                            <td><input type="checkbox"
+                                       name="wp_json_exporter_is_redirect" <?php checked( get_option( 'wp_json_exporter_is_redirect' ), 1 ); ?>
+                                       value="1"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Redirect URL:</th>
+                            <td><input type="text" name="wp_json_exporter_redirect_url"
+                                       value="<?php echo esc_attr( get_option( 'wp_json_exporter_redirect_url' ) ); ?>">
+                            </td>
+                        </tr>
+                    </table>
 
 					<?php submit_button(); ?>
-				</form>
-			</div>
+                </form>
+            </div>
 			<?php
 		}
 	}
