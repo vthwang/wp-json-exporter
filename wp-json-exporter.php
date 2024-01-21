06499b4415348de2dd28eb8ce6da7134a2367809
TingSyuanWang <vincent@fishboneapps.com>
2024-01-20 22:20:30 -0500

    chore: bump the version
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- wp-json-exporter.php
+++ wp-json-exporter.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WordPress JSON Exporter
  * Plugin URI:        https://github.com/vthwang/wp-json-exporter
  * Description:       Customized for blog systems, this plugin enables the export of your WordPress posts and projects in JSON format.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 6.4
  * Text Domain:       wp-json-exporter
  * Author:            Vincent Wang
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! class_exists( 'WPJsonExporter' ) ) {
-	define( 'WP_JSON_EXPORTER_VERSION', '1.0.1' );
+	define( 'WP_JSON_EXPORTER_VERSION', '1.0.2' );
 	define( 'WP_JSON_EXPORTER_DIR', __DIR__ );
 
 	/**
