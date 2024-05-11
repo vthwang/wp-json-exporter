f2f0e9ef19acc7a8f36357a7fdee4a11e59f1412
vthwang <vincent@fishboneapps.com>
2024-05-11 00:05:21 -0700

    refactor: add progress messages to the script
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- release.sh
+++ release.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+echo "🚀 Starting the script..."
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <version>"
     exit 1
 fi
 
 version=$1
+echo "✅ Version specified: $version"
 
+echo "📥 Cloning the repository..."
 git clone git@github.com:vthwang/wp-json-exporter.git
 cd wp-json-exporter || exit
 
+echo "🧹 Removing unnecessary files..."
 # Remove unnecessary files
 rm -rf .git .idea .github .gitignore phpcs.xml Release.md release.sh
 
 cd ..
+echo "📦 Creating zip file..."
 zip -r "wp-json-exporter-$version.zip" wp-json-exporter -x "*.DS_Store" -x "MACOSX"
+
+echo "🧽 Cleaning up..."
 rm -rf "wp-json-exporter"
+
+echo "🎉 Script completed successfully."
